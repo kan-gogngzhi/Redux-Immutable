d871348b0341af9e2c136ef5fc6ae3b61d703bac
Gongzhi Kan <kangongzhi@gmail.com>
2016-10-10 22:53:22 +1100

    eslint/webpack WIP

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Router, Route, hashHistory, IndexRoute} from 'react-router';
+import { Router, Route, hashHistory } from 'react-router';
 import {MainContainer} from '../containers';
+
 const routes = (
     <Router history={hashHistory}>
-        <Route path="/" component={MainContainer}>
-        </Route>
-        <Route path="*" component={MainContainer}/>
+        <Route path='/' component={MainContainer} />
+        <Route path='*' component={MainContainer}/>
     </Router>
 );
 
